dd39ea6479bdeaa87aa558cb5908c0726679a589
Sylvester <sylvestertrevi@gmail.com>
2023-01-04 03:20:06 -0500

    commit tree b72d999083d32cb9b4eaf3b81b5d8cae187052f7

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+function cats(){}
+cats = ["Milo", "Otis", "Garfield"];
+
+function arrayFunction (){
+    beforeEach(function () {
+        cats.length = 0;
+      cats.push("Milo", "Otis", "Garfield");
+      });
+      
+}
+function destructivelyAppendCat(){
+   cats.push("Ralph");   
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+function appendCat(name){
+    let cats = ["Milo","Otis","Garfield"];
+    let catsCopy = [...cats, name];
+    return catsCopy
+}
+function prependCat(name){
+    let cats = ["Milo","Otis","Garfield"];
+    let catsCopy = [name, ...cats];
+    return catsCopy
+}
+function removeLastCat(){
+    let cats = ["Milo","Otis","Garfield"];
+    let catsCopy = cats.slice(0, -1);
+    return catsCopy
+}
+function removeFirstCat(){
+    let cats = ["Milo","Otis","Garfield"];
+    let catsCopy = cats.slice(1);
+    return catsCopy
+}
+
